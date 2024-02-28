b7ed93b0aee90d0a480aa4224881f0c02fc28264
Erfan Azad <erfan.basiri.azad@gmail.com>
2024-02-28 15:57:46 +0100

    don't convert infos to numpy when loading datasets

--- src/imitation/data/huggingface_utils.py
+++ src/imitation/data/huggingface_utils.py
@@ -1,4 +1,5 @@
 """Helpers to convert between Trajectories and HuggingFace's datasets library."""
+
 from typing import Any, Dict, Iterable, Optional, Sequence, cast
 
 import datasets
@@ -18,7 +19,10 @@ class TrajectoryDatasetSequence(Sequence[types.Trajectory]):
         """Construct a TrajectoryDatasetSequence."""
 
         def numpy_transform(batch):
-            return {key: np.asarray(val) for key, val in batch.items()}
+            return {
+                key: np.asarray(val) if key != "infos" else val
+                for key, val in batch.items()
+            }
 
         # TODO: this is just a temporary workaround for
         #  https://github.com/huggingface/datasets/issues/5517
